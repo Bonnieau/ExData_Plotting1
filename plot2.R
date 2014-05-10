bda279563c9edb5077b9e7b9a118d41f033b1d0d
Bonnieau <b.hoschke@uws.edu.au>
2014-05-10 23:38:27 +1000

    Course Project 1 for Exploratory Data Analysis

--- plot2.R
+++ plot2.R
@@ -3,14 +3,15 @@ if(!file.exists("d:/rwork/exdata.zip"))
 {
 	download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","d:/rwork/exdata.zip")
 }
-setwd("d:/rwork")
 #read data
 exdata<-read.table(unz("exdata.zip",filename="household_power_consumption.txt"),header=TRUE,sep=";",na.strings="?")
 head(exdata)
 #extract data 1/2/2007 and 2/2/2007
 subdata<-exdata[which(exdata$Date=='1/2/2007'|exdata$Date=='2/2/2007'),]
 head(subdata)
+#convert Date and Time to Date/Time Class
+mydt<-strptime(paste(subdata$Date, subdata$Time), format='%d/%m/%Y %H:%M:%S')
 #draw plot to png file
-png("d:/rwork/plot1.png",width=480,height=480,units="px")
-hist(subdata$Global_active_power,,main="Global Active Power",xlab="Global Active Power (kilowatts)",col="red", ylim=c(0,1200))
+png("d:/rwork/plot2.png",width=480,height=480,units="px")
+plot(mydt, subdata$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
---
+++ plot3.R
@@ -0,0 +1,20 @@
+#download file to r working directory
+if(!file.exists("d:/rwork/exdata.zip"))
+{
+	download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","d:/rwork/exdata.zip")
+}
+#read data
+exdata<-read.table(unz("exdata.zip",filename="household_power_consumption.txt"),header=TRUE,sep=";",na.strings="?")
+head(exdata)
+#extract data 1/2/2007 and 2/2/2007
+subdata<-exdata[which(exdata$Date=='1/2/2007'|exdata$Date=='2/2/2007'),]
+head(subdata)
+#convert Date and Time to Date/Time Class
+mydt<-strptime(paste(subdata$Date, subdata$Time), format='%d/%m/%Y %H:%M:%S')
+#draw plot to png file
+png("d:/rwork/plot3.png",width=480,height=480,units="px")
+plot(mydt, subdata$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering",ylim=c(0,38),col="black")
+lines(mydt,subdata$Sub_metering_2, type="l",col="red")
+lines(mydt,subdata$Sub_metering_3, type="l",col="blue")
+legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
